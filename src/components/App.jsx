d00930e4e4d5d8f1aa4af5461b950864fa0e0f28
VladosSid <sidorsv121097@gmail.com>
2022-12-13 22:02:20 +0200

    update App

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,4 +1,4 @@
-import { Component } from 'react';
+import { useState, useEffect, useRef } from 'react';
 
 import api from '../helpers/api';
 
@@ -8,106 +8,198 @@ import ImageGallery from './ImageGallery';
 import ButtonLoadMore from './Button';
 import Loader from './Loader';
 
-export class App extends Component {
-  state = {
-    searchQuery: '',
-    pageNumber: 1,
-    dataRequest: [],
-    error: '',
-    loader: false,
-  };
+export function App() {
+  const [searchQuery, setSearchQuery] = useState('');
+  const [pageNumber, setPageNumber] = useState(1);
+  const [dataRequest, setDataRequest] = useState([]);
+  const [errorApi, setErrorApi] = useState('');
+  const [loader, setLoader] = useState(false);
 
-  onSearch = valueSearch => {
-    const { searchQuery } = this.state;
+  const notRequestFirstRender = useRef(true);
+
+  const onSearch = valueSearch => {
     if (searchQuery !== valueSearch) {
-      this.setState({ dataRequest: [], pageNumber: 1 });
+      setDataRequest([]);
+      setPageNumber(1);
     }
 
-    this.setState({ searchQuery: valueSearch });
+    setSearchQuery(valueSearch);
   };
 
-  increaseNumberPages = () => {
-    this.setState(prevState => ({
-      pageNumber: prevState.pageNumber + 1,
-    }));
-  };
-
-  componentDidUpdate(prevProps, prevState) {
-    const { loader } = this.state;
-
-    const { searchQuery, pageNumber } = this.state;
-    if (
-      prevState.searchQuery !== searchQuery ||
-      prevState.pageNumber !== pageNumber
-    ) {
-      this.requestApi();
-      this.setState({ loader: !loader });
-    }
-  }
-
-  requestApi = async () => {
-    const { pageNumber, searchQuery } = this.state;
+  const requestApi = async () => {
+    setLoader(!loader);
 
     try {
-      const { dataRequest } = this.state;
-
       const request = await api.fetchImagesWithQuery(searchQuery, pageNumber);
 
-      this.setState({
-        dataRequest: [...dataRequest, ...request.hits],
-      });
+      setDataRequest([...dataRequest, ...request.hits]);
     } catch (error) {
-      this.setState({ error });
+      setErrorApi(error.message);
+      console.log(errorApi);
     } finally {
-      const { loader } = this.state;
-      this.setState({ loader: !loader });
+      setLoader(!loader);
     }
   };
 
-  render() {
-    const { dataRequest, loader } = this.state;
-
-    return (
-      <div>
-        <Global
-          styles={css`
-            html {
-              box-sizing: border-box;
-              width: 100vw;
-              overflow-x: hidden;
-            }
-
-            *,
-            *::before,
-            *::after {
-              box-sizing: inherit;
-            }
-
-            body {
-              margin: 0;
-              font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto,
-                Oxygen-Sans, Ubuntu, Cantarell, 'Helvetica Neue', sans-serif;
-              color: #212121;
-              background-color: #fff;
-              -webkit-font-smoothing: antialiased;
-              -moz-osx-font-smoothing: grayscale;
-            }
-
-            img {
-              display: block;
-              max-width: 100%;
-              height: auto;
-            }
-          `}
-        />
-        {loader && <Loader />}
-
-        <Searchbar onSearch={this.onSearch} />
-        <ImageGallery dataApi={dataRequest} />
-        {dataRequest.length > 11 ? (
-          <ButtonLoadMore loadMore={this.increaseNumberPages} />
-        ) : null}
-      </div>
+  useEffect(() => {
+    if (notRequestFirstRender.current) {
+      notRequestFirstRender.current = false;
+      console.log('Отмена useEffect при первом рендере');
+      return;
+    }
+    console.log(
+      'useEffect при изменении searchQuery и последующим запросом на сервер'
     );
-  }
+    requestApi();
+  }, [searchQuery, pageNumber]);
+
+  return (
+    <div>
+      <Global
+        styles={css`
+          html {
+            box-sizing: border-box;
+            width: 100vw;
+            overflow-x: hidden;
+          }
+
+          *,
+          *::before,
+          *::after {
+            box-sizing: inherit;
+          }
+
+          body {
+            margin: 0;
+            font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto,
+              Oxygen-Sans, Ubuntu, Cantarell, 'Helvetica Neue', sans-serif;
+            color: #212121;
+            background-color: #fff;
+            -webkit-font-smoothing: antialiased;
+            -moz-osx-font-smoothing: grayscale;
+          }
+
+          img {
+            display: block;
+            max-width: 100%;
+            height: auto;
+          }
+        `}
+      />
+      {loader && <Loader />}
+
+      <Searchbar onSearch={onSearch} />
+      <ImageGallery dataApi={dataRequest} />
+      {dataRequest.length > 11 ? (
+        <ButtonLoadMore loadMore={setPageNumber(state => state + 1)} />
+      ) : null}
+    </div>
+  );
 }
+
+export default App;
+
+// export default function oldApp () {
+
+// state = {
+//   searchQuery: '',
+//   pageNumber: 1,
+//   dataRequest: [],
+//   error: '',
+//   loader: false,
+// };
+
+// onSearch = valueSearch => {
+//   const { searchQuery } = this.state;
+//   if (searchQuery !== valueSearch) {
+//     this.setState({ dataRequest: [], pageNumber: 1 });
+//   }
+
+//   this.setState({ searchQuery: valueSearch });
+// };
+
+// increaseNumberPages = () => {
+//   this.setState(prevState => ({
+//     pageNumber: prevState.pageNumber + 1,
+//   }));
+// };
+
+// componentDidUpdate(prevProps, prevState) {
+//   const { loader } = this.state;
+
+//   const { searchQuery, pageNumber } = this.state;
+//   if (
+//     prevState.searchQuery !== searchQuery ||
+//     prevState.pageNumber !== pageNumber
+//   ) {
+//     this.requestApi();
+//     this.setState({ loader: !loader });
+//   }
+// }
+
+// requestApi = async () => {
+//   const { pageNumber, searchQuery } = this.state;
+
+//   try {
+//     const { dataRequest } = this.state;
+
+//     const request = await api.fetchImagesWithQuery(searchQuery, pageNumber);
+
+//     this.setState({
+//       dataRequest: [...dataRequest, ...request.hits],
+//     });
+//   } catch (error) {
+//     this.setState({ error });
+//   } finally {
+//     const { loader } = this.state;
+//     this.setState({ loader: !loader });
+//   }
+// };
+
+//   render() {
+//     const { dataRequest, loader } = this.state;
+
+//     return (
+//       <div>
+//         <Global
+//           styles={css`
+//             html {
+//               box-sizing: border-box;
+//               width: 100vw;
+//               overflow-x: hidden;
+//             }
+
+//             *,
+//             *::before,
+//             *::after {
+//               box-sizing: inherit;
+//             }
+
+//             body {
+//               margin: 0;
+//               font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto,
+//                 Oxygen-Sans, Ubuntu, Cantarell, 'Helvetica Neue', sans-serif;
+//               color: #212121;
+//               background-color: #fff;
+//               -webkit-font-smoothing: antialiased;
+//               -moz-osx-font-smoothing: grayscale;
+//             }
+
+//             img {
+//               display: block;
+//               max-width: 100%;
+//               height: auto;
+//             }
+//           `}
+//         />
+//         {loader && <Loader />}
+
+//         <Searchbar onSearch={this.onSearch} />
+//         <ImageGallery dataApi={dataRequest} />
+//         {dataRequest.length > 11 ? (
+//           <ButtonLoadMore loadMore={this.increaseNumberPages} />
+//         ) : null}
+//       </div>
+//     );
+//   }
+// }
